414953efdb6e3e53f41912b1ccd933827fc8872a
David0403 <thanhtuvo135@gmail.com>
2024-04-17 17:05:40 +0700

    Update coco_eval.py

--- tool/tv_reference/coco_eval.py
+++ tool/tv_reference/coco_eval.py
@@ -5,8 +5,7 @@ import numpy as np
 import copy
 import time
 import torch
-import torch._six
-
+from torch import inf
 from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
 import pycocotools.mask as mask_util
